6bbd859c684e6c74358498dbdea1b56014409968
ammaar345 <fakirammaar@gmail.com>
2020-08-04 22:41:31 +0200

    added test for two function

---
+++ test/totalPhoneBill.test.js
@@ -0,0 +1,21 @@
+let assert=require("assert");
+let totalPhoneBill=require("../totalPhoneBill");
+describe("Calculates the total phone bill accordingly.", function() {
+  
+  
+    it("Should return the total phone bill for 2 calls and 3 sms's",function(){ 
+        var callSms="call, sms, call, sms, sms";
+        assert.equal(totalPhoneBill(callSms), "R"+7.45);
+    });
+    it("Should return the Total phone bill for 1 call and 1 SMS",function(){ 
+        var callSms="call, sms";
+    
+    assert.equal(totalPhoneBill(callSms),"R"+3.4.toFixed(2));
+});
+it("Should return the total phone bill for 2 sms's",function(){ 
+    var callSms="sms, sms";
+
+assert.equal(totalPhoneBill(callSms),"R"+1.3.toFixed(2));
+});
+
+});
---
+++ test/yearsAgo.test.js
@@ -0,0 +1,22 @@
+let assert=require("assert");
+let yearsAgo=require("../yearsAgo")
+describe("Calculate how many years ago from todays date to the given date.", function() {
+  
+  
+    it("Should return the amount of years as 44",function(){ 
+       var year=1976;
+        assert.equal(yearsAgo(year),44);
+        
+    });
+
+    it("Should return the amount of years as 34",function(){ 
+        var year=1986;
+         assert.equal(yearsAgo(year),34);
+         
+     });  
+       it("Should return the amount of years as 99",function(){ 
+        var year=1921;
+         assert.equal(yearsAgo(year),99);
+         
+     });
+});
